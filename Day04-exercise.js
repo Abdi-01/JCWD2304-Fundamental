5c7aa8fc8593716c19d4b5b11e159ae515690a0e
Abdi-01 <abdialghi@gmail.com>
2022-12-05 11:51:54 +0700

    day05-exercise

---
+++ Day04-exercise.js
@@ -0,0 +1,32 @@
+// PSEUDO CODE
+
+// INPUT
+// Parameter value height of pattern
+//  INPUT : param height, variable print
+
+// Proses
+// 1. 
+// 2.
+// 3.
+
+// Output
+// 01
+// 02 03
+// 04 05 06
+
+// ADD FUNCTION
+// INPUT : Parameter numb1, numb2
+
+// PROSES
+const addFunc = (numb1, numb2) => {
+    // 1. Get value from parameter
+    // 2. Aritmatich process for numb1 + numb2
+    let result = numb1 + numb2;
+    // 3. Send result to output
+
+    return result
+}
+
+// OUTPUT : result add
+
+console.log(addFunc(10, 20));
